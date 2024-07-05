ac6dfe0c2b21c5952449a7411146e67021647ccf
Eggze2 <3088371460@qq.com>
2024-07-05 23:54:52 +0800

    warning resolved

--- src/main/java/com/example/ros_web_control/RosWebControlApplication.java
+++ src/main/java/com/example/ros_web_control/RosWebControlApplication.java
@@ -8,11 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class RosWebControlApplication {
 
-	@RequestMapping("/hello")
-	public String index(){
-		return "Hello World， Spring boot is good";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(RosWebControlApplication.class, args);
 	}
